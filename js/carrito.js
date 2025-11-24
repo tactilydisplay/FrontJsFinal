afb63aec19c6d5d1d3ddf4bf53c4aaac06829b77
tactilydisplay <tactilydisplay@gmail.com>
2025-11-24 12:35:18 -0300

    modificacion carritojs

--- js/carrito.js
+++ js/carrito.js
@@ -3,31 +3,24 @@ import { eliminarProducto, vaciarCarrito } from "./funciones.js";
 import { actualizarContador } from "./ui.js";
 
 const renderizarCarrito = () => {
-  //Leemos cantidad de productos en carrito para mostrar
   const carrito = obtenerCarrito();
   actualizarContador(carrito);
 
-  //Accedemos al nodo donde vamos a mostrar las tarjetas de producto
   const contenedor = document.getElementById("contenedor-carrito");
-  // Botones de acciones
   const divAcciones = document.getElementById("acciones-carrito");
 
-  //Esta lineas limpian los contenedor antes de renderizar tarjetas y botones
   contenedor.innerHTML = "";
   divAcciones.innerHTML = "";
 
-  //❌Si no hay productos en el carrito mostramos un mensaje
   if (!carrito.length) {
     const mensaje = document.createElement("p");
     mensaje.classList.add("mensaje-carrito-vacio");
     mensaje.textContent = "No hay productos en el carrito";
     contenedor.appendChild(mensaje);
-    return; //⚠️salimos de la funcion para no intentar renderizar productos
+    return; 
   }
 
-  //✅Si hay productos en el carrito los renderizamos
-  //💡El forEach nos puede dar el indice del producto en el array
-  carrito.forEach((producto, indice) => {
+   carrito.forEach((producto, indice) => {
     const tarjeta = document.createElement("article");
     tarjeta.classList.add("tarjeta-producto");
 
@@ -45,13 +38,11 @@ const renderizarCarrito = () => {
     btnEliminar.classList.add("btn");
     btnEliminar.classList.add("btn-eliminar-carrito");
 
-    //💡Aca nos sirve el indice, para poder pasarselo a la funcion de eliminar
     btnEliminar.textContent = "Eliminar";
     btnEliminar.addEventListener("click", () => {
       eliminarProducto(indice);
 
-      //⚠️Importante! Volver a renderizar el carrito para actualizar la vista,
-      // ya que sino quedaria con el producto eliminado porque solo borramos del storage
+ 
       renderizarCarrito();
     });
 
@@ -70,8 +61,7 @@ const renderizarCarrito = () => {
   btnVaciar.addEventListener("click", () => {
     vaciarCarrito();
 
-    //⚠️Importante! Volver a renderizar el carrito para actualizar la vista,
-    // ya que sino quedaria con los productos viejos porque solo borramos del storage
+
     renderizarCarrito();
   });
 
